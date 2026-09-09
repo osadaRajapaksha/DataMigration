5c0fd5593ab965f401415a7a161a6f2a0995cfa1
Osada <osadanirmal9@gmail.com>
2026-09-09 12:34:15 +0530

    Add first and last name columns

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,3 +4,5 @@
 CREATE OR REPLACE TABLE `your-gcp-project-id.enterprise_analytics_dev.customers`
 (
     customer_id STRING OPTIONS(description="Unique identifier for the customer"),
+    first_name STRING,
+    last_name STRING,
