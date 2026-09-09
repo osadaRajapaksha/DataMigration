83ce579d0b3c3550ca2e03a117964e163c2f87a2
Osada <osadanirmal9@gmail.com>
2026-09-09 12:34:14 +0530

    Add output for BigQuery dataset ID

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "raw_bucket_name" {
   description = "Name of the GCS bucket for raw data"
   value       = google_storage_bucket.raw_data_lake.name
 }
+
+output "bigquery_dataset_id" {
+  description = "ID of the BigQuery Dataset"
+  value       = google_bigquery_dataset.analytics_dataset.dataset_id
+}
